6baa105b40460369766d76d334d44f803218509c
Vaibhavrahinj <70082433+Vaibhavrahinj@users.noreply.github.com>
2022-07-12 13:56:03 +0530

    New data added

--- array method.js
+++ array method.js
@@ -1,8 +1,19 @@
-let nums=[12,34,83,44,23,54];
+// for each method 1
+let nums=[18,34,83,44,23,54];
 nums.forEach((n,i,nums)=>{
     // first argument = n means value as like 12
     //second argument = i means index value 0,1,2,3 
     // third argument = nums means all array value or printed array value 
-    console.log(n,i,nums);
+    document.write(n);
     
 })
+// for each method 2
+var a=["rahul","akash","Nitin","Bharati"];
+
+a.forEach(feach);
+function feach(value,index){
+    console.log(index,value);
+    
+}
+
+//Reduce method in js
